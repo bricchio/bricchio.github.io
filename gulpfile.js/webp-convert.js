f875eb178a2437c604463d744e733d5563a3f86d
bricchio <bricchio@gmail.com>
2025-04-16 16:17:24 -0400

    New modules, images, file-updates

---
+++ gulpfile.js/webp-convert.js
@@ -0,0 +1,9 @@
+import gulp from 'gulp';
+import webp from 'gulp-webp';
+
+
+export default () => {
+    gulp.src('image-convert/original/*.{jpg,png}')
+		.pipe(webp())
+		.pipe(gulp.dest('image-convert/webp'))
+}
