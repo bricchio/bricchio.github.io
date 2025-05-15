0bd9c3017ba202ca4574b0882bf8d8dda87dfa65
bricchio <bricchio@gmail.com>
2025-05-14 20:57:38 -0400

    updates / fixes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 
 
-class headerTemplate extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-        <div id="header">
-            <div class="container">
-                <div class="flex-row flex-m-end flex-c-center">
-                    <a href="/index.html">Home</a>
-                    <a href="#about-me" target="">About Me</a>
-                    <a href="#portfolio">Projects</a>
-                    <a href="#work-history">Work History</a>
-                    <a href="#reviews">Reviews</a>
-                    <a href="#contact">Contact</a>
-                </div>
-            </div>
-        </div>
-        `;
-    }
-}
+// class headerTemplate extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <div id="header">
+//             <div class="container">
+//                 <div class="flex-row flex-m-end flex-c-center">
+//                     <a href="/index.html">Home</a>
+//                     <a href="#about-me" target="">About Me</a>
+//                     <a href="#portfolio">Projects</a>
+//                     <a href="#work-history">Work History</a>
+//                     <a href="#reviews">Reviews</a>
+//                     <a href="#contact">Contact</a>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//     }
+// }
 
-customElements.define('header-component', headerTemplate);
+// customElements.define('header-component', headerTemplate);
 
 window.addEventListener('scroll', function() {
     const header = document.getElementById('header');
